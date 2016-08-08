d3919c5a572ff1a7e342f22793d389ed47ead91e
parksw3 <parksw3@mcmaster.ca>
2016-08-08 14:16:07 -0400

    organization

--- R/prime_funs.R
+++
@@ -1,9 +0,0 @@
-is.prime <- function(x){
-    i <- 2:ceiling(sqrt(x))
-    for(i in i){
-        if(x %% i == 0){
-            return(FALSE)
-        }
-    }
-    return(TRUE)
-}
--- problems/problem003.R
+++ problems/problem003.R
@@ -1,4 +1,13 @@
-source("../R/prime_funs.R")
+is.prime <- function(x){
+    i <- 2:ceiling(sqrt(x))
+    for(i in i){
+        if(x %% i == 0){
+            return(FALSE)
+        }
+    }
+    return(TRUE)
+}
+
 n <- 600851475143
 i <- 2:ceiling(sqrt(n))
 f <- i[n %% i == 0]
